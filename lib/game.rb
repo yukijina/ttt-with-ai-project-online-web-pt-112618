357fde5ec2affcdd767802ea56646fddbed0fff7
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-02-07 04:07:45 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,10 @@
 class Game
+<<<<<<< HEAD
   attr_accessor :board, :player_1, :player_2
   attr_reader :current_player
+=======
+  attr_accessor :player_1, :player_2, :board
+>>>>>>> 867b86ad844a2e1eff6190ea76053fabaa3f6104
 
   WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]]
 
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,10 +1,15 @@
 module Players
-  class Human < Player
+ class Human < Player
 
     def move(board)
       input = gets.chomp
     end
+<<<<<<< HEAD
 
   end
+=======
+    
+  #end
+>>>>>>> 867b86ad844a2e1eff6190ea76053fabaa3f6104
 
 end
